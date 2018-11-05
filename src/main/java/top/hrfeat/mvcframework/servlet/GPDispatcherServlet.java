e470cb4d1b2c7ddd41f8195336674c5d7786fe04
chenhorin <chenhorin@163.com>
2018-11-05 15:18:27 +0800

    项目创建

---
+++ src/main/java/top/hrfeat/mvcframework/servlet/GPDispatcherServlet.java
@@ -0,0 +1,31 @@
+package top.hrfeat.mvcframework.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class GPDispatcherServlet extends HttpServlet {
+
+    public GPDispatcherServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+}
